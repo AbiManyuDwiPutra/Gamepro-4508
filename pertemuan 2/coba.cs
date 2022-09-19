2ea2be7ffd27f1ae8f89080705fd3140037b5622
AbiManyuDwiPutra <91584962+AbiManyuDwiPutra@users.noreply.github.com>
2022-09-19 21:55:36 +0700

    Add files via upload

---
+++ pertemuan 2/coba.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coba : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string myName;
+
+    void Start()
+    {
+        Debug.Log("I am alive and my name is " + myName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
